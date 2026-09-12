edb439dd2eeb6ecb86be3c42b8d1722f6d645034
Kiuliumov <miti.bania@gmail.com>
2026-09-12 08:18:23 +0300

    Apply formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,20 @@
-const js = require('@eslint/js');
+const js = require("@eslint/js");
 
 module.exports = [
-	js.configs.recommended,
-	{
-		files: ['**/*.js'],
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'commonjs',
-			globals: {
-				console: 'readonly',
-				process: 'readonly'
-			}
-		},
-		rules: {
-			'no-unused-vars': 'warn',
-			'no-undef': 'error'
-		}
-	}
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+    },
+  },
 ];
--- index.js
+++ index.js
@@ -2,11 +2,7 @@ require("dotenv").config();
 
 const fs = require("node:fs");
 const path = require("node:path");
-const {
-  Client,
-  Collection,
-  GatewayIntentBits,
-} = require("discord.js");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
