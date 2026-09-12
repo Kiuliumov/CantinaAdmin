d124f7acf76c95bc581cc04a68224a4b85b5c034
Kiuliumov <miti.bania@gmail.com>
2026-09-12 08:21:48 +0300

    Add ping command

--- eslint.config.js
+++ eslint.config.js
@@ -2,16 +2,24 @@ const js = require("@eslint/js");
 
 module.exports = [
   js.configs.recommended,
+
   {
     files: ["**/*.js"],
+
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs",
+
       globals: {
         console: "readonly",
         process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        require: "readonly",
+        module: "readonly",
       },
     },
+
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "error",
